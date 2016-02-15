df26b2dc1184241fb7abcc565dfa2947ae515d95
Patch <patchandthat@gmail.com>
2016-02-15 16:36:05 +0000

    Interface scaffolding

---
+++ Bootstrapper.Interface/CaliburnMicroBootstrapper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using Autofac;
+using Bootstrapper.Interface.UI;
+using Caliburn.Micro;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using StartupEventArgs = System.Windows.StartupEventArgs;
+
+namespace Bootstrapper.Interface
+{
+    public class CaliburnMicroBootstrapper : BootstrapperBase
+    {
+        private IContainer _container;
+        private readonly BootstrapperApplication _app;
+
+        public CaliburnMicroBootstrapper(BootstrapperApplication app)
+        {
+            LogManager.GetLog = type => new WixLog(app.Engine, type);
+
+            this._app = app;
+
+            Initialize();
+            SetupContainer();
+        }
+
+        private void SetupContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            RegisterCaliburnMicroFramework(builder);
+
+            builder.RegisterInstance<BootstrapperApplication>(_app).SingleInstance();
+
+            _container = builder.Build();
+        }
+
+        private void RegisterCaliburnMicroFramework(ContainerBuilder builder)
+        {
+            builder.RegisterType<WindowManager>().As<IWindowManager>().SingleInstance();
+            builder.RegisterType<EventAggregator>().As<IEventAggregator>().SingleInstance();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly)
+                   .Where(t => t.Name.EndsWith("ViewModel") || t.Name.EndsWith("View"))
+                   .AsSelf()
+                   .InstancePerDependency();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            var settings = new Dictionary<string, object>
+            {
+                { "SizeToContent", SizeToContent.Manual },
+                { "Height" , 350  },
+                { "Width"  , 500 },
+                { "ResizeMode", ResizeMode.NoResize }
+            };
+            DisplayRootViewFor<ShellViewModel>(settings);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            object instance;
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                if (_container.TryResolve(service, out instance))
+                    return instance;
+            }
+            else
+            {
+                if (_container.TryResolveNamed(key, service, out instance))
+                    return instance;
+            }
+            throw new Exception(string.Format("Could not locate any instances of contract {0}.", key ?? service.Name));
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.Resolve(typeof(IEnumerable<>).MakeGenericType(service)) as IEnumerable<object>;
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.InjectProperties(instance);
+        }
+
+        protected override IEnumerable<System.Reflection.Assembly> SelectAssemblies()
+        {
+            return new[] { Assembly.GetExecutingAssembly() };
+        }
+    }
+}
---
+++ Bootstrapper.Interface/UI/ShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+
+namespace Bootstrapper.Interface.UI
+{
+    class ShellViewModel : Screen
+    {
+        private readonly BootstrapperApplication _ba;
+
+        public ShellViewModel(BootstrapperApplication ba)
+        {
+            _ba = ba;
+        }
+
+        public void PassconfigPathToMsi()
+        {
+            _ba.Engine.StringVariables["DotaConfigDir"] = "THE PATH";
+
+            //Before
+            //_ba.Engine.Plan(LaunchAction.Install);
+        }
+    }
+}
