7d401b4480a632f7e410d4ed9bea6ca191b77f68
Patch <patchandthat@gmail.com>
2016-02-17 13:01:54 +0000

    Starting on installer Dir location & actual UI..

---
+++ Bootstrapper.Interface/UI/BusyViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Caliburn.Micro;
+
+namespace Bootstrapper.Interface.UI
+{
+    class BusyViewModel : Screen
+    {
+    }
+}
---
+++ Bootstrapper.Interface/Util/DotaDirectoryLocator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootstrapper.Interface.Util
+{
+    /*
+            Try to identify the dota2 directory.
+            Obvious choice is %programfiles%\Steam\Steamapps\common
+            Also check the root of each drive for a "SteamLibrary" dir
+            Else fallback to letting the user pick via dialog in bootstrapper ui
+            Ultimately looking for the "dota 2 beta\game\dota\cfg" directory.
+            Once found, should write this to registry to make uninstallation & updates easier
+
+            We could also automatically search the whole machine for the DOTA folder, 
+            display a progress indicator, and have a button for the user to manually specify the directory.
+            I much prefer it to be automated by default            
+    */
+
+    class DotaDirectoryLocator
+    {
+        public DirectoryLocationResult SearchForDotaDirectory()
+        {
+            //Todo:
+            //Async, support cancellation, indicate busy status, current directory, overall progress
+
+            throw new NotImplementedException();
+        }
+
+        public void SpecifyDirectoryManually(string path)
+        {
+            //Cancels the async operation, and uses the path provided as the Dota2 directory.
+            //Do some validation here, and possibly adjust the path so that we know we're in the right place
+        }
+    }
+
+    public class DirectoryLocationResult
+    {
+        public enum SearchOutcome
+        {
+            Undefined,
+            Found,
+            NotFound
+        }
+
+        public DirectoryLocationResult(SearchOutcome outcome)
+        {
+            Outcome = outcome;
+            Path = "";
+        }
+
+        public DirectoryLocationResult(SearchOutcome outcome, string path)
+        {
+            Outcome = outcome;
+            Path = path;
+        }
+
+        public SearchOutcome Outcome { get; }
+        public string Path { get; }
+    }
+}
